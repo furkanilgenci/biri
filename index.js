15796ea3bb0dd5ba470de8004f77dcf95dee7a5f
Furkan Ilgenci <furkanilgenci@gmail.com>
2020-10-03 15:45:16 +0300

    Return null when there is no RTCIceCandidate.foundation in any candidate

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-let _id = null
+let _id = undefined
 
 const promises = []
 
 let connection
 
+function resolvePromises(value) {
+  _id = value
+  promises.forEach(resolve => resolve(_id))
+  promises.length = 0
+  connection.removeEventListener('icecandidate', onIceCandidate)
+}
+
+function onIceCandidate({ candidate }) {
+  if (connection.iceGatheringState == 'complete' && _id == undefined) {
+    return resolvePromises(null)
+  }
+  if (candidate && candidate.foundation) {
+    return resolvePromises(candidate.foundation)
+  }
+}
+
 async function startConnection() {
   if (connection) return
 
-  const peerConnectionConfig = [
-    { sdpSemantics: "unified-plan" },
-    { sdpSemantics: "plan-b" }
-  ]
-
-  connection = new RTCPeerConnection(peerConnectionConfig[0]);
+  connection = new RTCPeerConnection();
 
   connection.addEventListener('icecandidate', onIceCandidate);
 
@@ -21,23 +32,13 @@ async function startConnection() {
     offerToReceiveVideo: 1
   })
   connection.setLocalDescription(offer)
-
-  function onIceCandidate({ candidate }) {
-    if (!candidate) return
-
-    if (candidate.protocol == 'udp') {
-      _id = candidate.foundation
-      promises.forEach(resolve => resolve(_id))
-      promises.length = 0
-    }
-  }
 }
 
 async function biri() {
   if (typeof RTCPeerConnection == 'undefined')
     throw new Error(`This browser doesn't support WebRTC, so biri cannot provide a unique, static ID for this machine.`)
 
-  if (_id) return _id
+  if (typeof _id != 'undefined') return _id
 
   const promise = new Promise(resolve => {
     startConnection()
